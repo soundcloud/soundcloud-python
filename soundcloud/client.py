b87adaae153cb888937a6e420e96e9314e9281ce
Paul Osman <paul@eval.ca>
2012-02-08 23:15:42 -0500

    Add support for various OAuth protocol flows

--- soundcloud/client.py
+++ soundcloud/client.py
@@ -11,23 +11,87 @@ class Client(object):
 
     use_ssl = True
     host = 'api.soundcloud.com'
-    auth_kwargs = ['client_id', 'client_secret', 'access_token',
-                   'redirect_uri', 'username', 'password']
 
     def __init__(self, **kwargs):
         """Create a client instance with the provided options. Options should
         be passed in as kwargs.
         """
-        for kwarg in self.auth_kwargs:
-            if kwarg not in kwargs:
-                continue
-            setattr(self, kwarg, kwargs.get(kwarg))
+        self.use_ssl = kwargs.get('use_ssl', self.use_ssl)
+        self.scheme = self.use_ssl and 'https://' or 'http://'
+        self.options = kwargs
 
-    def _resolve_resource_name(self, name):
-        """Convert a resource name (e.g. tracks) into a URI."""
-        name = name.rstrip('/').lstrip('/')
-        scheme = self.use_ssl and 'https://' or 'http://'
-        return '%s%s/%s.json' % (scheme, self.host, name)
+        self.client_id = kwargs.get('client_id')
+
+        if 'access_token' in kwargs:
+            self.access_token = kwargs.get('access_token')
+            return
+
+        if 'client_id' not in kwargs:
+            raise TypeError("At least a client_id must be provided.")
+
+        if 'scope' in kwargs:
+            self.scope = kwargs.get('scope')
+
+        # decide which protocol flow to follow based on the arguments
+        # provided by the caller.
+        if self._options_for_authorization_code_flow_present():
+            self._authorization_code_flow()
+        elif self._options_for_credentials_flow_present():
+            self._credentials_flow()
+        elif self._options_for_token_refresh_present():
+            self._refresh_token_flow()
+
+    def exchange_token(self, code):
+        """Given the value of the code parameter, request an access token."""
+        url = '%s%s/oauth2/token' % (self.scheme, self.host)
+        options = {
+            'grant_type': 'authorization_code',
+            'redirect_uri': self.options.get('redirect_uri'),
+            'client_id': self.options.get('client_id'),
+            'client_secret': self.options.get('client_secret'),
+            'code': code,
+        }
+        self.token = wrapped_resource(
+            requests.post(url, data=options))
+        self.access_token = self.token.access_token
+        return self.token
+
+    def _authorization_code_flow(self):
+        """Build the the auth URL so the user can authorize the app."""
+        options = {
+            'scope': getattr(self, 'scope', 'non-expiring'),
+            'client_id': self.options.get('client_id'),
+            'response_type': 'code',
+            'redirect_uri': self.options.get('redirect_uri')
+        }
+        url = '%s%s/connect' % (self.scheme, self.host)
+        self.authorize_url = '%s?%s' % (url, urlencode(options))
+
+    def _refresh_token_flow(self):
+        """Given a refresh token, obtain a new access token."""
+        url = '%s%s/oauth2/token' % (self.scheme, self.host)
+        options = {
+            'grant_type': 'refresh_token',
+            'client_id': self.options.get('client_id'),
+            'client_secret': self.options.get('client_secret'),
+            'refresh_token': self.options.get('refresh_token')
+        }
+        self.token = wrapped_resource(
+            requests.post(url, data=options))
+        self.access_token = self.token.access_token
+
+    def _credentials_flow(self):
+        """Given a username and password, obtain an access token."""
+        url = '%s%s/oauth2/token' % (self.scheme, self.host)
+        options = {
+            'client_id': self.options.get('client_id'),
+            'client_secret': self.options.get('client_secret'),
+            'username': self.options.get('username'),
+            'password': self.options.get('password'),
+            'grant_type': 'password'
+        }
+        self.token = wrapped_resource(requests.post(url, data=options))
+        self.access_token = self.token.access_token
 
     def _request(self, method, resource, **kwargs):
         """Given an HTTP method, a resource name and kwargs, construct a
@@ -36,10 +100,10 @@ class Client(object):
         request_func = getattr(requests, method)
         url = self._resolve_resource_name(resource)
 
-        for kwarg in self.auth_kwargs:
-            if getattr(self, kwarg, None) is None:
-                continue
-            kwargs[kwarg] = getattr(self, kwarg)
+        if hasattr(self, 'access_token'):
+            kwargs = dict(oauth_token=self.access_token)
+        if hasattr(self, 'client_id'):
+            kwargs.update(dict(client_id=self.client_id))
 
         if method == 'get':
             qs = urlencode(kwargs)
@@ -54,3 +118,25 @@ class Client(object):
         if name not in ['get', 'post', 'put', 'head', 'delete']:
             raise AttributeError
         return partial(self._request, name)
+
+    def _resolve_resource_name(self, name):
+        """Convert a resource name (e.g. tracks) into a URI."""
+        name = name.rstrip('/').lstrip('/')
+        return '%s%s/%s.json' % (self.scheme, self.host, name)
+
+    # Helper functions for testing arguments provided to the constructor.
+
+    def _options_present(self, options, kwargs):
+        return all(map(lambda k: k in kwargs, options))
+
+    def _options_for_credentials_flow_present(self):
+        required = ('client_id', 'client_secret', 'username', 'password')
+        return self._options_present(required, self.options)
+
+    def _options_for_authorization_code_flow_present(self):
+        required = ('client_id', 'redirect_uri')
+        return self._options_present(required, self.options)
+
+    def _options_for_token_refresh_present(self):
+        required = ('client_id', 'client_secret', 'refresh_token')
+        return self._options_present(required, self.options)
---
+++ soundcloud/tests/test_oauth.py
@@ -0,0 +1,86 @@
+from contextlib import contextmanager
+from urllib import urlencode
+
+from nose.tools import eq_
+
+import fudge
+import soundcloud
+
+from soundcloud.tests.utils import MockResponse
+
+
+@contextmanager
+def non_expiring_token_response(fake_http_request):
+    response = MockResponse(
+        '{"access_token":"access-1234","scope":"non-expiring"}')
+    fake_http_request.expects_call().returns(response)
+    yield
+
+
+@contextmanager
+def expiring_token_response(fake_http_request):
+    response = MockResponse(
+        '{"access_token":"access-1234","expires_in":12345,"scope":"*",' +
+        '"refresh_token":"refresh-1234"}')
+    fake_http_request.expects_call().returns(response)
+    yield
+
+
+@contextmanager
+def positive_refresh_token_response(fake_http_request):
+    response = MockResponse(
+        '{"access_token":"access-2345","expires_in":21599,"scope":"*",' +
+        '"refresh_token":"refresh-2345"}')
+    fake_http_request.expects_call().returns(response)
+    yield
+
+
+def test_authorize_url_construction():
+    """Test that authorize url is being generated properly."""
+    client = soundcloud.Client(client_id='foo', client_secret='bar',
+                               redirect_uri='http://example.com/callback')
+    eq_('https://api.soundcloud.com/connect?%s' % (urlencode({
+        'scope': 'non-expiring',
+        'client_id': 'foo',
+        'response_type': 'code',
+        'redirect_uri': 'http://example.com/callback'
+     }),), client.authorize_url)
+
+
+@fudge.patch('requests.post')
+def test_exchange_code_non_expiring(fake):
+    """Test that exchanging a code for an access token works."""
+    with non_expiring_token_response(fake):
+        client = soundcloud.Client(client_id='foo', client_secret='bar',
+                                   redirect_uri='http://example.com/callback')
+        token = client.exchange_token('this-is-a-code')
+        eq_('access-1234', token.access_token)
+        eq_('non-expiring', token.scope)
+        eq_('access-1234', client.access_token)
+
+
+@fudge.patch('requests.post')
+def test_exchange_code_expiring(fake):
+    """Excluding a scope=non-expiring arg should generate a refresh token."""
+    with expiring_token_response(fake):
+        client = soundcloud.Client(client_id='foo', client_secret='bar',
+                                   redirect_uri='http://example.com/callback',
+                                   scope='*')
+        eq_('https://api.soundcloud.com/connect?%s' % (urlencode({
+            'scope': '*',
+            'client_id': 'foo',
+            'response_type': 'code',
+            'redirect_uri': 'http://example.com/callback'
+        }),), client.authorize_url)
+        token = client.exchange_token('this-is-a-code')
+        eq_('access-1234', token.access_token)
+        eq_('refresh-1234', token.refresh_token)
+
+
+@fudge.patch('requests.post')
+def test_refresh_token_flow(fake):
+    """Providing a refresh token should generate a new access token."""
+    with positive_refresh_token_response(fake):
+        client = soundcloud.Client(client_id='foo', client_secret='bar',
+                                   refresh_token='refresh-token')
+        eq_('access-2345', client.token.access_token)
