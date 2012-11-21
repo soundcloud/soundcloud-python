f27ae4fc6e8b82ebd4a8a1556743b2222cec1752
Paul Osman <paul@eval.ca>
2012-11-21 15:29:22 +0100

    Raising an HTTPError for a 3xx is no longer desired

--- soundcloud/tests/test_requests.py
+++ soundcloud/tests/test_requests.py
@@ -9,9 +9,16 @@ from requests.exceptions import HTTPError
 from soundcloud.tests.utils import MockResponse
 
 
+class MockRaw(object):
+    """Simple mock for the raw response in requests model."""
+    def __init__(self):
+        self.reason = "foo"
+
+
 @contextmanager
 def response_status(fake_http_request, status):
     response = MockResponse('{}', status_code=status)
+    response.raw = MockRaw()
     fake_http_request.expects_call().returns(response)
     yield
 
@@ -20,6 +27,7 @@ def response_status(fake_http_request, status):
 def test_bad_responses(fake):
     """Anything in the 400 or 500 range should raise an exception."""
     client = soundcloud.Client(client_id='foo', client_secret='foo')
+
     for status in range(400, 423):
         with response_status(fake, status):
             assert_raises(HTTPError, lambda: client.get('/me'))
@@ -35,15 +43,3 @@ def test_ok_response(fake):
         with response_status(fake, status):
             user = client.get('/me')
 
-@fudge.patch('requests.get')
-def test_redirects(fake):
-    """Make sure 300 responses raise an exception.
-
-    Note: ```requests``` transparently attempts redirects so if we get
-    this back it means the caller has disabled redirects or the max
-    number has been reached.
-    """
-    client = soundcloud.Client(client_id='foo', client_secret='foo')
-    for status in (300, 301, 302, 303, 304, 305, 307):
-        with response_status(fake, status):
-            assert_raises(HTTPError, lambda: client.get('/me'))
