fe08dbd0476b85c4bf5c693d5cf282e73e71dc23
Arthur Maciejewicz <arm32@njit.edu>
2013-02-03 11:26:22 -0500

    By default, MockResponse reason is OK

--- soundcloud/tests/utils.py
+++ soundcloud/tests/utils.py
@@ -2,7 +2,7 @@ from requests.models import Response
 
 
 class MockResponse(Response):
-    def __init__(self, content, status_code=200, url=None, reason=None):
+    def __init__(self, content, status_code=200, url=None, reason='OK'):
         self._content = content
         self.status_code = status_code
         self.url = url
