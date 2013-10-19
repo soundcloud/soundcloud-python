12230adc7a1645afbac8e4795b98ecef1d15ef5f
Keith Hughitt <keith.hughitt@gmail.com>
2013-10-19 07:55:07 -0400

    Decoded response using response.encoding

--- soundcloud/resource.py
+++ soundcloud/resource.py
@@ -48,16 +48,19 @@ def wrapped_resource(response):
     Lists will be returned as a ```ResourceList``` instance,
     dicts will be returned as a ```Resource``` instance.
     """
+    # decode response text
+    response_content = response.content.decode(response.encoding)
+
     try:
-        content = json.loads(response.content)
+        content = json.loads(response_content)
     except ValueError:
         # not JSON
-        content = response.content
+        content = response_content
     if isinstance(content, list):
         result = ResourceList(content)
     else:
         result = Resource(content)
-    result.raw_data = response.content
+    result.raw_data = response_content
 
     for attr in ('url', 'status_code', 'reason'):
         setattr(result, attr, getattr(response, attr))
