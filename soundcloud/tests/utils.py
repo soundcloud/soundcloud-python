9e857fb752796efa02c80a8ccaa2ac8b7e3aa2cd
Keith Hughitt <keith.hughitt@gmail.com>
2013-10-19 08:52:58 -0400

    Added response encoding in mock Request

--- soundcloud/tests/utils.py
+++ soundcloud/tests/utils.py
@@ -2,9 +2,9 @@ from requests.models import Response
 
 
 class MockResponse(Response):
-    def __init__(self, content, encoding='utf-8', status_code=200, url=None,
-                 reason='OK'):
-        self._content = content
+    def __init__(self, content, encoding='utf-8',
+                 status_code=200, url=None, reason='OK'):
+        self._content = content.encode('utf-8')
         self.encoding = encoding
         self.status_code = status_code
         self.url = url
